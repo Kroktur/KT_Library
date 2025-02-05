83acda322f86193ccfe35f1a115afc6d121495e1
Arthur Blanchet <arthur.blanchet.16@gmail.com>
2025-02-05 16:55:30 +0100

    need fix fusion sort

--- KT_Library/include/KT_FusionSort.h
+++ KT_Library/include/KT_FusionSort.h
@@ -5,6 +5,7 @@ namespace KT
 	struct FusionSort
 	{
 		using Iterator = container::iterator;
+		using difference_type = container::iterator::difference_type;
 		void operator()(container& data)
 		{
 			internalfusion(data, data.begin(), data.end());
@@ -20,62 +21,52 @@ namespace KT
 			auto curentrightidx = rightstartidx;
 
 			auto tabidx = 0;
-			/*auto leftendidx = middleidx;
-			auto rightstartidx = middleidx + 1;
-			int curentleftidx = startidx;
-			int curentrightidx = rightstartidx;
-			int tabidx = 0;
-
-			int tabsize = (leftendidx - startidx + 1) + (endrightidx - rightstartidx + 1);
-			int* tmptab = new int[tabsize];*/
 
 			auto tabsize = endrightidx - startidx + 1;
-			//int tabsize = (leftendidx - startidx + 1) + (endrightidx - rightstartidx + 1);
 			auto tmptab = val;
 
 			while (curentleftidx != leftendidx + 1 || curentrightidx != endrightidx + 1)
 			{
 				if (curentleftidx == leftendidx + 1)
 				{
-					tmptab[tabidx] = val[curentrightidx];
+					tmptab[tabidx] = *curentrightidx;
 					++tabidx;
 					++curentrightidx;
 				}
 				else if (curentrightidx == endrightidx + 1)
 				{
-					tmptab[tabidx] = val[curentleftidx];
+					tmptab[tabidx] = *curentleftidx;
 					++tabidx;
 					++curentleftidx;
 				}
 				else
 				{
-					if (val[curentleftidx] <= val[curentrightidx])
+					if (*curentleftidx <= *curentrightidx)
 					{
-						tmptab[tabidx] = val[curentleftidx];
+						tmptab[tabidx] = *curentleftidx;
 						++tabidx;
 						++curentleftidx;
 					}
 					else
 					{
-						tmptab[tabidx] = val[curentrightidx];
+						tmptab[tabidx] = *curentrightidx;
 						++tabidx;
 						++curentrightidx;
 					}
 				}
 			}
-			for (auto i = 0; i < tabsize; ++i)
+			/*for (auto i = 0; i < tabsize; ++i)
 			{
 				val[i + startidx] = tmptab[i];
-			}
-			delete[] tmptab;
-			tmptab = nullptr;
+			}*/
+
 		}
-		void internalfusion(container& val, const Iterator& startidx, const Iterator& endidx)
+		void internalfusion(container& val,  Iterator startidx, Iterator endidx)
 		{
 			if (startidx == endidx)
 				return;
 
-			auto middleidx = (endidx + startidx) / 2;
+			auto middleidx = startidx +(/*(endidx + startidx) /*/ 2);
 
 			internalfusion(val, startidx, middleidx);
 			internalfusion(val, middleidx + 1, endidx);
